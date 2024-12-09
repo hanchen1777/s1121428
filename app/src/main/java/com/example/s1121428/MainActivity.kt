04df5dc8f3c0465006b479e0840b63537eec1248
hanchen1777 <hy60712@gmail.com>
2024-12-09 16:21:38 +0800

    v2_楊涵真

--- app/src/main/java/com/example/s1121428/MainActivity.kt
+++ app/src/main/java/com/example/s1121428/MainActivity.kt
@@ -4,14 +4,33 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.s1121428.ui.theme.S1121428Theme
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults.buttonColors
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,19 +48,43 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    S1121428Theme {
-        Greeting("Android")
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xff95fe95)),
+    ){
+        Column (
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Text(
+                text = "\n2024期末上機考(資管二B楊涵真)"
+            )
+            Image(
+                painter = painterResource(id = R.drawable.class_b),
+                contentDescription = "資管二B",
+                modifier = Modifier
+            )
+            Text(
+                text = "遊戲持續時間 0 秒"
+            )
+            Text(
+                text = "您的成績0分"
+            )
+            Button(
+                onClick = {
+                        System.exit(0)
+                },
+                colors = buttonColors(Color.Black)
+            ) {
+                Text(
+                    text = "結束APP",
+                    color = Color.White
+                )
+            }
+        }
     }
 }
